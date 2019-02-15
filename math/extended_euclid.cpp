a0ab076f657625b545f578036eea6869ce8134c8
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-02-16 00:12:38 +0600

    Update extended_euclid.cpp

--- math/extended_euclid.cpp
+++ math/extended_euclid.cpp
@@ -87,6 +87,63 @@ bool find_one_solution(int a,int b,int c,int &x0,int &y0,int &g){
     return true;
 }
 
+void shift_solution(int &x,int &y,int a,int b,int cnt){
+    x+=cnt*b;
+    y-=cnt*a;
+}
+
+///range: minx<=x<=maxx,miny<=y<=maxy
+
+int find_all_solutions(int a,int b,int c,int minx,int maxx,int miny,int maxy){
+    int x,y,g;
+    if(!find_one_solution(a,b,c,x,y,g))
+        return 0;
+
+    a/=g,b/=g;
+
+    int sign_a=a>0? 1:-1;
+    int sign_b=b>0? 1:-1;
+
+    shift_solution(x,y,a,b,(minx-x)/b);
+
+    if(x<minx)
+        shift_solution(x,y,a,b,sign_b);
+    if(x>maxx)
+        return 0;
+    int lx1=x;
+
+    shift_solution(x,y,a,b,(maxx-x)/b);
+
+    if(x>maxx)
+        shift_solution(x,y,a,b,-sign_b);
+    int rx1=x;
+
+    shift_solution(x,y,a,b,-(miny-y)/a);
+    if(y<miny)
+        shift_solution(x,y,a,b,-sign_a);
+    if(y>maxy)
+        return 0;
+    int lx2=x;
+
+    shift_solution(x,y,a,b,-(maxy-y)/a);
+    if(y>maxy)
+        shift_solution(x,y,a,b,sign_a);
+
+    int rx2=x;
+
+    if(lx2>rx2)
+        swap(lx2,rx2);
+
+    int lx=max(lx1,lx2);
+    int rx=min(rx1,rx2);
+    if(lx>rx) return 0;
+
+    return (rx-lx)/abs(b) +1;
+
+    ///solutions can be found
+    ///x=lx+k*b and y from ax+by=c: iterate!
+}
+
 int main()
 {
     //FI;FO;
